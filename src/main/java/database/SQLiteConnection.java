3ba55233490149ea91f7815592b9f307ee03c35b
Edgaras Mackevičius <edasmackis@gmail.com>
2023-11-22 12:27:05 +0200

    sqlite
    Signed-off-by: Edgaras Mackevičius <edasmackis@gmail.com>

--- src/main/java/database/SQLiteConnection.java
+++ src/main/java/database/SQLiteConnection.java
@@ -1,24 +1,56 @@
 package database;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 public class SQLiteConnection {
-    public static void main(String[] args) {
-        Connection connection = null;
+    private static final String URL = "jdbc:sqlite:/home/edgaras/Desktop/BankingApplication/BankingProject/src/main/java/database/database.db";
+    private Connection connection;
+
+    public SQLiteConnection() {
         try {
-            String url = "jdbc:sqlite:src/main/java/database/database.db";
-            connection = DriverManager.getConnection(url);
+            connection = DriverManager.getConnection(URL);
             System.out.println("Connection established");
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
+    }
+
+    public void closeConnection() {
+        if (connection != null) {
             try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
+                connection.close();
+                System.out.println("Connection closed");
+            } catch (SQLException e) {
+                System.out.println("Failed to close connection: " + e.getMessage());
             }
         }
+    }
+
+    public void selectAll(String tableName) {
+        String sql = "SELECT * FROM " + tableName;
+
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                // Assuming your table has an 'id', 'name', and 'balance' column
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                double balance = rs.getDouble("balance");
+                System.out.println(id + "\t" + name + "\t" + balance);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        SQLiteConnection sqliteConnect = new SQLiteConnection();
+        // Replace 'your_table_name' with your actual table name
+        sqliteConnect.selectAll("BankAccounts");
+        sqliteConnect.closeConnection();
     }
 }
