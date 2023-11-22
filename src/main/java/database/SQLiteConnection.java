77be6f57fe2baf0caa908bc058f915eafea03b16
Edgaras Mackevičius <edasmackis@gmail.com>
2023-11-22 10:50:13 +0200

    sqlite
    Signed-off-by: Edgaras Mackevičius <edasmackis@gmail.com>

--- src/main/java/database/SQLiteConnection.java
+++ src/main/java/database/SQLiteConnection.java
@@ -3,5 +3,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class SQLiteConnection {
-
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            String url = "jdbc:sqlite:src/main/java/database/database.db";
+            connection = DriverManager.getConnection(url);
+            System.out.println("Connection established");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 }
